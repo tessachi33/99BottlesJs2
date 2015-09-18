2798e7c4c85d634a0ce19518b58ac42d0efffb69
Tessa <Tessachi33@yahoo.com>
2015-09-18 13:44:45 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,10 @@
 
 var getBottles = function(amount) {
    for(var i = amount; i > 0; i--) {
-    var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of absinthe, take a slug and pass it down, " + (i -1) + " bottles of absinthe on the wall. " + " No more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ";
+    var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of absinthe, take a slug and pass it down, " + (i - 1) + " bottles of absinthe on the wall. " + " No more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ";
     }
 
-  return bottlesAmount ;
+  return bottlesAmount + " 'this is a test' ";
 };
 
 $(document).ready(function() {
