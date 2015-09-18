1c395b1f6c455ae976dd9a17bc355f6d9664d0af
Tessa <Tessachi33@yahoo.com>
2015-09-18 12:17:49 -0700

    recogni=zing getBottles, but inouts are wrong, getting close

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,21 @@
 // Take one down, pass it around
 // 0 bottles of beer on the wall
 
-var getBottles = function(amount){
-   for(var i = amount; i > 0; i--){
-    var bottles =(i + " bottles of absinthe on the wall " + i + " bottles of absinth, take a slug and pass it down " + (i -1) + " bottles of absinthe " + " no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ");
-    }
+var getBottles = function(amount) {
+   for(var i = amount; i > 0; i--) {
+var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of absinth, take a slug and pass it down " + (i -1) + " bottles of absinthe " + " no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ";
   }
 
-  return bottles;
+  return bottlesAmount;
 };
 
 $(document).ready(function() {
   $("form#bottle").submit(function() {
     var amount = ($("input#amount").val());
-    $(".song").text(bottles);
-    $("#results").show();
-    event.preventDefault();
+
+    $(".bottles").text(getBottles);
+    $(".result").show();
+     event.preventDefault();
 
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('getBottles', function() {
   it("will get the next bottle number", function() {
-    expect(getBottles(99)).to.equal(98);
+    expect(getBottles(1)).to.equal("1 bottles of absinthe on the wall 1 bottles of absinth, take a slug and pass it down 0 bottles of absinthe  no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe" );
   });
 });
