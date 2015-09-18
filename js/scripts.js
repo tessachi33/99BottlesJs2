21e4740ac5ec737961e1057c10bc6f775533b356
Tessa <Tessachi33@yahoo.com>
2015-09-18 14:42:09 -0700

    Add arra to documnt ready, still notprinting

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,30 @@
 // Take one down, pass it around
 // 0 bottles of beer on the wall
 
-var getBottles = function(amount) {
-   for(var i = amount; i > 0; i--) {
-    var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of absinthe, take a slug and pass it down, " + (i - 1) + " bottles of absinthe on the wall. " + " No more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ";
-    }
+var bottles1 = " bottles of absinthe on the wall ";
+var bottles2 = " bottles of absinthe, take a slug and pass it down, ";
+var bottles3 =  " bottles of absinthe on the wall. ";
+var lastBottle =  " No more bottles of absinthe on the wall, go to the grave, you cannot drink more absinthe "
 
-  return bottlesAmount + " 'this is a test' ";
+var getBottles = function(amount) {
+
+   for(var i = amount; i > 0; i--) {
+
+     var bottlesAmount[];
+
+      bottlesAmount.push(i + bottles1 + i + bottles2 + (i - 1) + bottles3);
+
+      bottlesAmount.push(lastBottle);
+
+        }
+
+  return bottlesAmount + " 'this is a test' ";  //+ " 'this is a test' "
 };
 
 $(document).ready(function() {
   $("form#bottles").submit(function() {
     var amount = parseInt($("input#amount").val());
+    var bottlesAmount = getBottles(amount);
 
     $(".results").show();
     $(".absinthe").text(getBottles);
