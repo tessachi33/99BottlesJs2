472e4643712b1aedeb557aa84e3db6ff006c9308
Tessa <Tessachi33@yahoo.com>
2015-09-18 12:25:33 -0700

    First test passes, text doesn't show

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@
 
 var getBottles = function(amount) {
    for(var i = amount; i > 0; i--) {
-var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of absinth, take a slug and pass it down " + (i -1) + " bottles of absinthe " + " no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ";
+var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of absinthe, take a slug and pass it down " + (i -1) + " bottles of absinthe " + " no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe ";
   }
 
   return bottlesAmount;
@@ -18,9 +18,9 @@ var bottlesAmount = i + " bottles of absinthe on the wall " + i + " bottles of a
 
 $(document).ready(function() {
   $("form#bottle").submit(function() {
-    var amount = ($("input#amount").val());
+    var amount = ($("input#number").val());
 
-    $(".bottles").text(getBottles);
+    $(".song").text(bottlesAmount);
     $(".result").show();
      event.preventDefault();
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('getBottles', function() {
   it("will get the next bottle number", function() {
-    expect(getBottles(1)).to.equal("1 bottles of absinthe on the wall 1 bottles of absinth, take a slug and pass it down 0 bottles of absinthe  no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe" );
+    expect(getBottles(1)).to.equal("1 bottles of absinthe on the wall 1 bottles of absinthe, take a slug and pass it down 0 bottles of absinthe  no more bottles of absinthe on the wall, go to the grave you cannot drink more absinthe " );
   });
 });
